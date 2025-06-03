079c31fff6c4b95e1bdb0b8a447762a21cf9d608
Augusto Salim <augustosalim98@gmail.com>
2025-06-03 14:55:03 -0300

    inicio de proyecto v2

--- ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d32267a35053a7347d652c2e4f744b22c83ad0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c7bb701d73f19d62e9facf8257b80e17f05fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
